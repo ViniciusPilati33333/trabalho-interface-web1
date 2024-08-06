26fe6446b9186b88b0d50162b8bec1b7d81705d9
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-08-05 22:03:11 -0300

    criando componentes do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Input } from "./components/ui/input"
+import { Label } from "./components/ui/label"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid w-full max-w-sm items-center gap-1.5 justify-center">
+      <div className="flex gap-3">
+        <div>
+          <Label>First name</Label>
+          <Input />
+        </div>
+
+        <div>
+          <Label>Last name</Label>
+          <Input />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+
+
+      <Label>Company Name(Optional)</Label>
+      <Input />
+
+      <Label>Country / Region</Label>
+      <Input />
+
+      <Label>Street Adress</Label>
+      <Input />
+
+       <Label>Town City</Label>
+      <Input />
+
+       <Label>Province</Label>
+      <Input />
+
+       <Label>Zip Code</Label>
+      <Input />
+
+       <Label>Phone</Label>
+      <Input />
+
+       <Label>Email Adress</Label>
+      <Input />
+
+      <Input placeholder="Aditional information"/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
